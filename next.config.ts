9997038821235f87a70444921eff0b26ec7b01de
Raymond Tjahyadi <raymondtjahyadi01@gmail.com>
2025-03-12 21:48:34 +0700

    Update agar Image Next Js dapat berjalan pada heroku

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+    domains: ['localhost'],
+  },
+  output: 'standalone',
   devIndicators: false
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 import React, { useState, useEffect } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
